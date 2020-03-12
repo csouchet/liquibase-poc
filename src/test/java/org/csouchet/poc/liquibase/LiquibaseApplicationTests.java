a818bc39469f7b65d088234ff8c67b3ba02eda38
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-12 10:04:42 +0100

    Init project with Spring boot + Liquibase + H2

---
+++ src/test/java/org/csouchet/poc/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csouchet.poc.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
