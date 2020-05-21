9bf4e4ec0361490b2f909dfd49d35e7a7f0708eb
Céline Souchet <souchet.celine.5@gmail.com>
2020-05-21 16:45:43 +0200

    Configure SpringBoot to use the test configuration of Liquibase, when the tests are launched

--- src/test/java/org/csouchet/poc/liquibase/LiquibaseApplicationTests.java
+++ src/test/java/org/csouchet/poc/liquibase/LiquibaseApplicationTests.java
@@ -2,12 +2,14 @@ package org.csouchet.poc.liquibase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LiquibaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
