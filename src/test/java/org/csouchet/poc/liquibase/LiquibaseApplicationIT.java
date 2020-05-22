7a31c131a240ac359a5b36bb109f29ac453a205c
Céline Souchet <souchet.celine.5@gmail.com>
2020-05-22 18:07:54 +0200

    Add configuration for IT & Unit test

---
+++ src/test/java/org/csouchet/poc/liquibase/LiquibaseApplicationIT.java
@@ -0,0 +1,15 @@
+package org.csouchet.poc.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("it")
+class LiquibaseApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
